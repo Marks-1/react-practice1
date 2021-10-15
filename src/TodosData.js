6e36a1604a467ad54cb3f7a61c670c283db2a5f2
Marks-1 <onyachmark1@gmail.com>
2021-10-15 18:19:03 +0300

    2 files added

---
+++ src/TodosData.js
@@ -0,0 +1,34 @@
+const TodosData = [
+  {
+    id: 1,
+    task: 'Clean the washroom',
+    completed: true
+  },
+  {
+    id: 2,
+    task: 'Take the trash out',
+    completed: false
+  },
+  {
+    id: 3,
+    task: 'Go to the grocery',
+    completed: false
+  },
+  {
+    id: 4,
+    task: 'Go to the gym',
+    completed: true
+  },
+  {
+    id: 5,
+    task: 'Work on the user story for the project',
+    completed: false
+  },
+  {
+    id: 6,
+    task: 'Push the assigned task feature to the github',
+    completed: true
+  }
+]
+
+export default TodosData
